3eb985e5fa79ed6b00706e1333175bda271fbab6
DhanoHacks <draman9224@gmail.com>
2025-04-26 20:32:49 +0530

    Added sphinx docs

---
+++ docs/source/SQLRanges.rst
@@ -0,0 +1,29 @@
+SQLRanges package
+=================
+
+Submodules
+----------
+
+SQLRanges.queries module
+------------------------
+
+.. automodule:: SQLRanges.queries
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+SQLRanges.utils module
+----------------------
+
+.. automodule:: SQLRanges.utils
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: SQLRanges
+   :members:
+   :show-inheritance:
+   :undoc-members:
---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SQLRanges'
+copyright = '2025, Dhananjay Raman, Saket Choudhary'
+author = 'Dhananjay Raman, Saket Choudhary'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # for Google-style or NumPy-style docstrings
+    'sphinx.ext.viewcode',  # (optional) adds "view source" links
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
