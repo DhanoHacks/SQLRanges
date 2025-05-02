39a9e6c83dc79b384caaaff72bec415ec733e19c
DhanoHacks <draman9224@gmail.com>
2025-05-02 13:28:16 +0530

    updated test for subtracted exons

--- tests/test_sqlranges.py
+++ tests/test_sqlranges.py
@@ -165,7 +165,6 @@ def test_subtracted_exons(sqlr: sqlranges, tmp_path: pathlib.Path):
         FROM mouse WHERE Feature = 'exon' 
         GROUP BY Chromosome, Strand
     """)
-    df1 = sqlr.subtract_intervals(sqlranges(other_genes, table_name="mouse", db_name=str(tmp_path / "test3.db")), feature_filter="exon", other_feature_filter="exon")
-    df2 = pd.read_csv("tests/expected_outputs/subtracted_exons_mouse.csv")
-    diff = pd.concat([df1, df2]).drop_duplicates(keep=False)
-    assert diff.empty
+    sqlr.subtract_intervals(sqlranges(other_genes, table_name="mouse", db_name=str(tmp_path / "test3.db")), feature_filter="exon", other_feature_filter="exon").to_csv(tmp_path / "subtracted_exons.csv", index=False)
+    diff = os.popen(f"diff {tmp_path}/subtracted_exons.csv tests/expected_outputs/subtracted_exons_mouse.csv").read()
+    assert diff == ""
